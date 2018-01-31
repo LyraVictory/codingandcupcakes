176dc54d1920d66ed4b8898b48b0099bc5882c5f
lyrat16 <lyrat16@gmail.com>
2018-01-30 23:26:52 -0500

    changed scripts.js to fix TypeError

--- scripts.js
+++ scripts.js
@@ -1,8 +1,8 @@
 (function() {
-   var headerHeight = document.getElementsByClassName('header')[0].clientHeight;
+  var headerHeight = document.getElementsByClassName('header')[0].clientHeight;
 	var instructions = document.getElementById('instructions');
 	var navItem = document.getElementById('show-instructions');
-	instructions.style.top = headerHeight + 'px';
+	// instructions.style.top = headerHeight + 'px';
 
 	navItem.addEventListener("click", function() {
 		if(document.getElementsByClassName('show-menu').length === 0) {
